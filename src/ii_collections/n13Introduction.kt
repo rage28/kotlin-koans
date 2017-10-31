3128c3243cdee3b2332ecc78c24a3bd4f0082d62
Raghavendra Bhuvan <rage28@gmail.com>
2017-10-31 19:10:35 +0530

    Pass test ii_13

--- src/ii_collections/n13Introduction.kt
+++ src/ii_collections/n13Introduction.kt
@@ -1,7 +1,5 @@
 package ii_collections
 
-import java.util.*
-
 /*
  * This part of workshop was inspired by:
  * https://github.com/goldmansachs/gs-collections-kata
@@ -10,15 +8,9 @@ import java.util.*
 /*
  *  There are many operations that help to transform one collection into another, starting with 'to'
  */
-fun example0(list: List<Int>) {
-    list.toSet()
-
-    list.toCollection(HashSet<Int>())
-}
 
 fun Shop.getSetOfCustomers(): Set<Customer> {
     // Return a set containing all the customers of this shop
-    todoCollectionTask()
-//    return this.customers
+    return customers.toSet()
 }
 
